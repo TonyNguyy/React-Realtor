1a52dae7135bd48884f07950917a58333d2720dd
Tony <102321536+TonyNguyy@users.noreply.github.com>
2023-08-06 14:00:44 -0400

    Create header components and routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
 import Offers from "./pages/Offers"
+import Header from "./components/Header"
 
 
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
     <Router>
+      <Header/>
         <Routes>
           <Route path="/" element= {<Home/>} />
           <Route path="/profile" element= {<Profile/>} />
