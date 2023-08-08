3f6daf99d36b82f87823895a029ac028dfe60cbf
Tony <102321536+TonyNguyy@users.noreply.github.com>
2023-08-08 13:53:29 -0400

    complete the oauth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-import {FcGoogle}from "react-icons/fc"
+import{getAuth, signInWithPopup} from "firebase/auth"
+import {FcGoogle} from "react-icons/fc"
+import {toast} from "react-toastify"
+import { GoogleAuthProvider } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
 export default function OAuth() {
+  const navigate = useNavigate()
+
+  async function onGoogleClick(){
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth,provider)
+      const user = result.user 
+
+      // Check if user already exist
+
+      const docRef = doc(db, "user", user.uid)
+      const docSnap = await getDoc(docRef)
+      if(!docSnap.exists()){
+        await setDoc(docRef, {
+          name:user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        })
+      }
+      navigate('/');
+
+    } catch (error) {
+      toast.error("Not authorized with Google")
+   
+    }
+
+  }
+
   return (
-    <button className='flex
+    <button type="button" onClick={onGoogleClick}
+    className='flex
     items-center justify-center w-full
     bg-red-700 text-white
     px-7 py-3 uppercase 
